082447198f353e6fcbcabb99a5f735c64afe513b
scpark81 <scpark81@poscoict.com>
2022-06-30 15:00:04 +0900

    test log 추가

--- src/main/java/com/psc/cloud1/Cloud1Application.java
+++ src/main/java/com/psc/cloud1/Cloud1Application.java
@@ -1,12 +1,16 @@
 package com.psc.cloud1;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Log4j2
 @SpringBootApplication
 public class Cloud1Application {
 
     public static void main(String[] args) {
+
+        log.info("cloud-1 start");
         SpringApplication.run(Cloud1Application.class, args);
     }
 
