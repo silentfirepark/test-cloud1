845c0255cc3a566a76fa838fb0957083764da5a5
scpark81 <scpark81@poscoict.com>
2022-06-30 13:56:45 +0900

    requestmapping 추가

--- src/main/java/com/psc/cloud1/controller/HelloController.java
+++ src/main/java/com/psc/cloud1/controller/HelloController.java
@@ -1,9 +1,11 @@
 package com.psc.cloud1.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/cloud1")
 public class HelloController {
 
     @GetMapping("/hello")
